b9c2f4c255959a9b230cee1f8c2864b48bf8f208
Jacob Schwind <jacobx1@gmail.com>
2019-02-10 18:24:07 -0500

    Style updates for 2019
        - Top header, always
        - center content
        - Shrink sticky header w/ to-top button
        - Remove unused template
        - Various tweaks

--- themes/jacobx1/assets/js/scripts.js
+++ themes/jacobx1/assets/js/scripts.js
@@ -1,3 +1,27 @@
-$(function(){
-    $('.prettySocial').prettySocial();
+$(function() {
+  $(".prettySocial").prettySocial();
+
+  const pastScrollPos = pix =>
+    document.body.scrollTop > pix || document.documentElement.scrollTop > pix;
+
+  const withinScrollPos = pix =>
+    document.body.scrollTop < pix && document.documentElement.scrollTop < pix;
+
+  window.onscroll = () => {
+    const sidebar = document.getElementsByClassName('sidebar')[0];
+    if (pastScrollPos(180)) {
+      if (!sidebar.classList.contains('scrolled')) {
+        sidebar.classList.add('scrolled');
+      }
+    } else if (withinScrollPos(10)) {
+      sidebar.classList.remove('scrolled');
+    }
+  };
+
+  document.getElementById('back-to-top').onclick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 });
